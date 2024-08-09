713207c9479e2d0042eb6f248f86cf99ab56e509
Guilherme Rodrigues <157857247+GuilhermeRodOliveira@users.noreply.github.com>
2024-08-09 20:52:06 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIOS ESTRUTURA CONDICIONAIS 7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace EXERCICIOS_ESTRUTURA_CONDICIONAIS_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 7 - Escreva um programa que peça ao usuário para inserir um número de mês(1 - 12) e, em seguida, imprima o nome do mês correspondente.
+            Console.WriteLine("Digite um número entre 1-12 e vou informar o mês correspondente:");
+            int mes = int.Parse(Console.ReadLine());
+
+            switch (mes)
+            {
+                case 1:
+                    Console.WriteLine("Janeiro");
+                    break;
+                case 2:
+                    Console.WriteLine("Fevereiro");
+                    break;
+                case 3:
+                    Console.WriteLine("Março");
+                    break;
+                case 4:
+                    Console.WriteLine("Abril");
+                    break;
+                case 5:
+                    Console.WriteLine("Maio");
+                    break;
+                case 6:
+                    Console.WriteLine("Junho");
+                    break;
+                case 7:
+                    Console.WriteLine("Julho");
+                    break;
+                case 8:
+                    Console.WriteLine("Agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("Setembro");
+                    break;
+                case 10:
+                    Console.WriteLine("Outubro");
+                    break;
+                case 11:
+                    Console.WriteLine("Novembro");
+                    break;
+                case 12:
+                    Console.WriteLine("Dezembro");
+                    break;
+                default:
+                    Console.WriteLine("Número de mês inválido. Digite um número entre 1-12.");
+                    break;
+            }
+        }
+    }
+}
